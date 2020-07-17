02ab9fba80a13b4e5202e1f153e2cb05ff6e83bc
Alex Andrew <pytormal@live.com>
2020-07-16 19:16:18 -0700

    completed task2

--- index.js
+++ index.js
@@ -30,13 +30,21 @@ class Airplane {
 /*
   TASK 1
     - Write a Person class whose constructor initializes `name` and `age` from arguments.
+
     - All instances of Person should also initialize with an empty `stomach` array.
+
     - Give instances of Person the ability to `.eat("someFood")`:
+
         + When eating an edible, it should be pushed into the `stomach`.
+
         + The `eat` method should have no effect if there are 10 items in the `stomach`.
+
     - Give instances of Person the ability to `.poop()`:
+
         + When an instance poops, its `stomach` should empty.
+
     - Give instances of Person a method `.toString()`:
+
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
@@ -81,19 +89,67 @@ console.log(person1.toString());
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
     - All instances built with Car:
+
         + should initialize with a `tank` at 0
+
         + should initialize with an `odometer` at 0
+
     - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
+
     - Give cars ability to `.drive(distance)`. The distance driven:
+
         + Should cause the `odometer` to go up.
-        + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+
+        + Should cause the the `tank` to go down 
+        taking `milesPerGallon` into account.
+
     - A car which runs out of `fuel` while driving can't drive any more distance:
+
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0
+    this.odometer = 0
+  }
+  
+  fill(gallons) {
+    
+    console.log(`is holding ${this.tank} gallons`)
+    this.tank += gallons;
+    console.log(`is holding ${this.tank} gallons`)
+    return
+  }
+  drive(distance) {
+    const milesUntilEmpty = this.tank * this.milesPerGallon;
+    
+    if (distance > milesUntilEmpty) {
+      this.odometer += milesUntilEmpty;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+    else {
+      this.odometer = + distance;
+    }
+  }
   
 }
+console.log(" - - - Task2")
+let Beetle = new Car("Beetle", 32)
+
+console.log(Beetle)
+console.log()
+console.log(Beetle.fill(12))
+console.log()
+console.log(Beetle)
+console.log()
+console.log(Beetle.drive(400))
+console.log()
+console.log(Beetle)
+
 
 /*
   TASK 3
@@ -107,7 +163,9 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
-class Lambdasian {}
+class Lambdasian {
+  
+}
 
 /*
   TASK 4
